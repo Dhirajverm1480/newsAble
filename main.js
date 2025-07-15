b6ff80d457acd74229ae970ef95efed5b967367a
Dhiraj Verma <144448841+Dhirajverm1480@users.noreply.github.com>
2025-07-15 12:42:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function displayData(Data) {
                             <p class="author">${article.author}</p>
                             <p class="title">${article.title}</p>
                             <p class="discription">${article.description}</p>
-                            <a href="${article.url}"> Click here</a>
+                            <a href="${article.url}">Read More</a>
                         </div>
                     </a>
     `;
